8df892514ead4466cc84c23cc636c57640ac08a5
Anil CELIK <96571618+AnilTheKillerCoder@users.noreply.github.com>
2022-07-22 08:34:16 +0200

    projet node-farm etape 1

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -45,24 +45,60 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 ////////////////////////////////////////////////////////////////////////
 // SERVER CONFIG, ROUTING
 
-// Il ne sera appelé q'une seul fois dans tout notre code
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    output = output.replace(/{%ID%}/g, product.id)
+    if(!product.organic) 
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+    return output
+}
+
+
+// Il ne sera appelé q'une seul fois dans tout notre code
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
+// console.log(dataObj)
 
 
 const server = http.createServer((req, res) => {
     // console.log(req.url)
     const pathName = req.url;
-    if (pathName === '/overview') {
-        res.end('THIS is OVERVIEW') // => Facons la plus simple de retourner une reponse
+
+    // Overview Page
+    if ((pathName === '/') || (pathName === '/overview')) {
+        res.writeHead(200, {
+            'ContentType': 'text/html'
+        })
+
+        const cardHTML = dataObj.map(element =>replaceTemplate(tempCard,element)).join('')
+        console.log(cardHTML);
+
+        const output = tempOverview.replace('{%PRODUCT_CARD%}', cardHTML)
+        
+        res.end(output) // => Facons la plus simple de retourner une reponse
+
+    // Product Page
     } else if (pathName === '/products') {
         res.end('THIS is PRODUCTS') // => Facons la plus simple de retourner une reponse
+
+    // API Page    
     } else if (pathName === '/API') {
         res.writeHead(200, {
             'ContentType': 'application/json'
         })
         res.end(data);
+     
+    // PAGE NOT FOUND    
     } else {
         res.writeHead(404, {
             'ContentType': 'text/html',
